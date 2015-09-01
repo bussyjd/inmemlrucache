9f4513429d752c315b0e9971a5f86e43bd96ccff
jeandaniel.bussy <jeandaniel.bussy@gree.net>
2015-09-01 18:20:45 +0900

    wip: lru implementation

--- app.go
+++ app.go
@@ -6,36 +6,39 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-// The structure of the LRU cache
+/* The structure of the LRU cache
+**** We need a doubly linked list to be able to promote and the entries in memory with best performance
+**** http://bigocheatsheet.com/
+ */
 type LRUCache struct {
 	size int
 	l    *list.List
-	// implement cache
 }
 
-type entry struct {
-	key       int
-	imagepath string
-}
+//type entry struct {
+//	key       int
+//	imagepath string
+//}
 
 func main() {
 	router := mux.NewRouter()
-	//l := list.New()
-	//l.Init()
-	// Default index page
+	// INIT TMPFS
+
+	lru := New(10)
 	router.HandleFunc("/", Index)
-	// Get(Key) Return the value assisiated with a key if it exists Otherwise returns 404
-	router.HandleFunc("/get/{id}", GetCache)
 	// Set(Key,Value) Set a new item in the cache
-	//	router.HandleFunc("/set/{cache}", SetCache)
+	router.HandleFunc("/set/{id}", SetCache(lru))
+	// Get(Key) Return the value assisiated with a key if it exists Otherwise returns 404
+	router.HandleFunc("/get/{id}", GetCache(lru))
 	//	// Delete an iem of the cache
 	//	router.HandleFunc("/del/{id}", DelCache)
 	//	// Reset() Delete all the items of the cache
-	router.HandleFunc("/reset", ResetCache)
+	//router.HandleFunc("/reset", ResetCache)
 	//	// Count() Returns the item count of the cache
 	//	router.HandleFunc("/count", ItemClount)
 	// Remove oldest
@@ -48,29 +51,82 @@ func New(size int) *LRUCache {
 	return &LRUCache{
 		size: size,
 		l:    list.New(),
-		// We build a hash table
-		////cache: make(map[])
 	}
 }
 
-func (lru *LRUCache) Add(key int, image string) {
+// GET
+func GetCache(lru *LRUCache) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		key, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			// invalid string
+		}
 
+		fmt.Fprintf(w, "Size: %v, key: %v", lru.size, key)
+		if key > lru.size {
+			fmt.Fprintf(w, "Key is superior to cache size")
+			// 404 The cache sie if of 10 entries
+		}
+		fmt.Printf("Lengh: %v", lru.l.Len())
+		// I have to search through the Doubly-Linked list at O(n)
+		i := 0
+		for e := lru.l.Front(); e != nil; e = e.Next() {
+			i++
+			fmt.Printf("Key %v", key)
+			fmt.Printf("i %v", i)
+			if key == i {
+				//imgpwd := e.Value
+				//fmt.Printf(imgpwd)
+			}
+		}
+		/*
+			// Read the image from the tmpfs
+			image, exists := tmpfs.read(imgpwd)
+			if exists == false {
+				return nil
+			}
+			// promote the item
+			lru.list.MovetoFront(key)
+			return image
+		*/
+	}
+}
+
+// SET
+func SetCache(lru *LRUCache) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		key, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			// invalid string
+		}
+		buff := []byte("Hi\n")
+		fmt.Fprintf(w, "Set Cache %v", key)
+		lru.Set(1, buff)
+	}
+}
+
+// LRUSET
+func (lru *LRUCache) Set(key int, image []byte) {
+	// First we check if the LRU cache is not full
+	fmt.Println(lru.l)
+	lru.l.Len()
+	//lru.l.PushFront("imagename.jpg")
+	//tmpfs.write(image, "imagename.jpg")
 	//if lru.cache == nil {
 	//}
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-}
-
-func GetCache(w http.ResponseWriter, r *http.Request) {
-	// promote the item
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-}
-
+/*
+// RESET
 func ResetCache(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "List flushed")
 }
 
 //func (lru *LRUCache) Add(int key, [byte] data) {
 //}
+*/
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
