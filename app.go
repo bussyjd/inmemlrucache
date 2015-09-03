16b0276c7405c5a364d3043d2a657db6ed7ff6f6
bussyjd <silversurfer972@gmail.com>
2015-09-03 17:54:29 +0900

    todo get tmpfs, size checking

--- app.go
+++ app.go
@@ -4,6 +4,8 @@ import (
 	"container/list"
 	"fmt"
 	"html"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -27,20 +29,19 @@ type LRUCache struct {
 
 func main() {
 	router := mux.NewRouter()
-	// INIT TMPFS
-
+	TmpfsInit()
 	lru := New(10)
 	router.HandleFunc("/", Index)
 	// Set(Key,Value) Set a new item in the cache
 	router.HandleFunc("/set/{id}", SetCache(lru))
 	// Get(Key) Return the value assisiated with a key if it exists Otherwise returns 404
 	router.HandleFunc("/get/{id}", GetCache(lru))
-	//	// Delete an iem of the cache
-	router.HandleFunc("/del/{id}", DelCache)
-	//	// Reset() Delete all the items of the cache
-	//router.HandleFunc("/reset", ResetCache)
+	// Delete an iem of the cache
+	router.HandleFunc("/del/{id}", DelCache(lru))
+	// Reset() Delete all the items of the cache
+	router.HandleFunc("/reset", ResetCache(lru))
 	//	// Count() Returns the item count of the cache
-	//	router.HandleFunc("/count", ItemClount)
+	router.HandleFunc("/count", ItemCount(lru))
 	// Remove oldest
 	//  router.HandleFunc("/del/oldest", DelOld)
 	log.Fatal(http.ListenAndServe(":8080", router))
@@ -52,6 +53,70 @@ func New(size int) *LRUCache {
 		size: size,
 		l:    list.New(),
 	}
+}
+
+// SET
+func SetCache(lru *LRUCache) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		key, err := strconv.Atoi(vars["id"])
+		// Set ID cannot be > 10
+		if key >= lru.size {
+			fmt.Fprintf(w, "Cache size limited to 10\n")
+			return
+		}
+		// Use io.LimitReader instead to limit data loading
+		//buf, _ := ioutil.ReadAll(r.Body)
+		//reader := bytes.NewReader([]byte(r.Body))
+		mbreader := io.LimitReader(r.Body, 1000000)
+		buf, err := ioutil.ReadAll(mbreader)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// TODO Avoid the file to be zero (check r.Body size)
+		// TODO Avoid the file to be over 1Mb
+		//imgrdsize, err := mbreader.Read(buf)
+		//if err == io.EOF {
+		//	fmt.Println(err)
+		//	fmt.Fprintf(w, "Image size exeeded %v \n", imgrdsize)
+		//	return
+		//}
+		// update the doubly linked list
+		if err != nil {
+			// invalid string
+		}
+		// Retireve the image data from POST
+		fmt.Fprintf(w, "Set Cache %v", key)
+		// Check if the status of the cache
+		switch {
+		case lru.l.Len() == lru.size:
+			oldest := lru.l.Back()
+			// tmpfsDel(oldest.value)
+			lru.l.Remove(oldest)
+			lru.Set(vars["id"], buf)
+		case lru.l.Len() < lru.size:
+			lru.Set(vars["id"], buf)
+		}
+
+		i := 0
+		for e := lru.l.Front(); e != nil; e = e.Next() {
+			i++
+			if key == lru.size {
+				fmt.Printf("Cache is full deleting oldest entry\n")
+				lru.l.Remove(e)
+				break
+			}
+		}
+	}
+}
+
+// LRUSET
+func (lru *LRUCache) Set(key string, image []byte) {
+	// First we check if the LRU cache is not full
+	lru.l.PushFront("imagename" + key + ".jpg\n")
+	fmt.Println(lru.l.Len())
+	//tmpfs.write(image, "imagename.jpg")
+	TmpfsWrite(image, key)
 }
 
 // GET
@@ -68,62 +133,70 @@ func GetCache(lru *LRUCache) http.HandlerFunc {
 			fmt.Fprintf(w, "Key is superior to cache size")
 			// 404 The cache sie if of 10 entries
 		}
-		fmt.Printf("Lengh: %v", lru.l.Len())
 		// I have to search through the Doubly-Linked list at O(n)
 		i := 0
 		for e := lru.l.Front(); e != nil; e = e.Next() {
-			i++
-			fmt.Printf("Key %v", key)
-			fmt.Printf("i %v", i)
+			if key == lru.size {
+				break
+			}
 			if key == i {
-				//imgpwd := e.Value
-				//fmt.Printf(imgpwd)
+				// Return the image
+				// TODO tmpfsRead
+				fmt.Fprintf(w, "Image: %v\n", e.Value)
+				// Promote the accessed entry
+				fmt.Fprintf(w, "Moving %v to Front", key)
+				lru.l.MoveToFront(e)
 			}
+			i++
 		}
-		/*
-			// Read the image from the tmpfs
-			image, exists := tmpfs.read(imgpwd)
-			if exists == false {
-				return nil
-			}
-			// promote the item
-			lru.list.MovetoFront(key)
-			return image
-		*/
 	}
 }
 
-// SET
-func SetCache(lru *LRUCache) http.HandlerFunc {
+// DEL
+func DelCache(lru *LRUCache) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		key, err := strconv.Atoi(vars["id"])
 		if err != nil {
 			// invalid string
 		}
-		// Retireve the image data from POST
-		buff := []byte("Hi\n")
-		fmt.Fprintf(w, "Set Cache %v", key)
-		lru.Set(1, buff)
+		i := 0
+		for e := lru.l.Front(); e != nil; e = e.Next() {
+			i++
+			if key == i {
+				imgpwd := e.Value
+				fmt.Printf("Image path to delete: %s", imgpwd)
+				// Remove the image in the tmpfs
+				// TmpfsDel(e.Value)
+				// Remove the image in the double-linked-list
+				lru.l.Remove(e)
+				break
+			}
+		}
+
+		// Remove the lru entry
+		//lru.Remove()
+		fmt.Println(key)
 	}
 }
 
-// LRUSET
-func (lru *LRUCache) Set(key int, image []byte) {
-	// First we check if the LRU cache is not full
-	lru.l.PushFront("imagename.jpg")
-	fmt.Println(lru.l.Len())
-	//tmpfs.write(image, "imagename.jpg")
+// COUNT
+func ItemCount(lru *LRUCache) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		i := 0
+		for e := lru.l.Front(); e != nil; e = e.Next() {
+			i++
+			fmt.Printf("Key:  %v\n", i)
+			fmt.Printf("Value:  %v\n", e.Value)
+		}
+	}
 }
 
-// DEL
-func DelCache(lru *LRUCache) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.request) {
-		vars := mux.Vars(r)
-		key, err := strconv.Atoi(vars["id"])
-		if err != nil {
-			// invalid string
-		}
+// RESET
+func ResetCache(lru *LRUCache) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		lru.l.Init()
+		fmt.Fprintf(w, "Cache flushed")
 	}
 }
 
--- tmpfs.go
+++ tmpfs.go
@@ -1,6 +1,63 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
 
-func Tmpfs() {
+const lrudir = "/tmp/lru"
+
+func TmpfsInit() {
+	fmt.Printf("Initializing tmpfs")
+	mkdir := exec.Command("mkdir", lrudir)
+	err := mkdir.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//log.Printf("Command finished with error: %v", err)
+	//mount := exec.Command("mount", "-t", "tmpfs", "-o", "size=20m", "tmpfs", "/tmp/lru/")
+	//errmount := mount.Start()
+	//if err != nil {
+	//	log.Fatal(errmount)
+	//}
+}
+
+func TmpfsWrite(buf []byte, id string) {
+	fdest, err := os.Create(lrudir + "/uploadedfile" + id + ".jpg")
+	if err != nil {
+		fmt.Printf("Unable to create the file for writing. Check your write access privilege")
+		panic(err)
+	}
+	defer fdest.Close()
+	// write in the file
+	wrote, err := fdest.Write(buf)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("wrote %d bytes\n", wrote)
+}
+
+// Remove specified file in the FS
+func TmpfsRm() {
+}
+
+//Clears the tmpfs FS
+func TmpfsClear() {
+}
+
+func TmpfsDestroy() {
+	rm := exec.Command("rm", "-rf", lrudir)
+	err := rm.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Command finished with error: %v", err)
+	umount := exec.Command("umount", lrudir)
+	errumount := umount.Start()
+	if errumount != nil {
+		log.Fatal(errumount)
+	}
+
 }
