3eefafab65451b010ca4619741ac6e79ba6b26dd
bussyjd <silversurfer972@gmail.com>
2015-09-06 19:52:22 +0900

    update doc, container working

--- app.go
+++ app.go
@@ -67,7 +67,7 @@ func SetCache(lru *LRUCache, buf []byte) (bool, error) {
 	if len(buf) == 0 {
 		return false, fmt.Errorf("Image size is empty")
 	}
-	newfilename := randStr(lrusizelimit) + ".jpg"
+	newfilename := randStr(lrusizelimit)
 	lrulen := lru.l.Len()
 	switch {
 	case lrulen == lru.size:
